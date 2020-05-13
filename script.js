9c62aa75d99a2b209f74a021060baa87e6a06eaa
harshgoel05 <harshgoel05@gmail.com>
2020-05-13 20:19:01 +0530

    inital

---
+++ script.js
@@ -0,0 +1,11 @@
+// console.log('Hello world');
+
+// // Event Listner - Click
+// document.getElementById('btn1').addEventListener('click', function myfun() {
+// 	document.getElementById('headingtechtable').innerHTML = Date();
+// });
+
+// // document.getElementById('btn1').addEventListener('click', function myfun() {
+// // 	console.log('Hello World');
+// // 	// document.getElementsByClassName('headingtechtable2').innerHTML = 'Hey its changed!';
+// // });
